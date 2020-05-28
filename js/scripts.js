b1e9a2765d7cdbcaf2ce12d9f117c70305f93f82
edwin <edogwel@gmail.com>
2020-05-28 08:56:54 +0300

    updated js file

--- js/scripts.js
+++ js/scripts.js
@@ -169,7 +169,7 @@ $(document).ready(function () {
 
       if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
 
-        $("#finallmessage").append(person + ", we have recieved your order and it is being processed.We will deliver at " + location + ". The total cost is sh. " + deliceryamount);
+        $("#finallmessage").append(person + ", we have recieved your order and it is being processed.We will deliver at " + location + ". The total cost is sh. " + deliveryamount);
         $("#totalbill").hide();
         $("#finallmessage").slideDown(1200);
       }
